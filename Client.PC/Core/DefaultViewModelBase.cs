eab94401719fe2ee3794a36215aa20f67fc7f64f
dishiyicijinqiu <458141536@qq.com>
2016-04-08 12:24:25 +0800

    123

---
+++ Client.PC/Core/DefaultViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FengSharp.OneCardAccess.Client.PC.Core
+{
+    public class DefaultViewModelBase : ViewModelBase
+    {
+        public DefaultViewModelBase()
+        {
+            this.PropertyChanged += DefaultViewModelBase_PropertyChanged;
+        }
+        protected bool HasPropertyChanged { get; set; }
+        private void DefaultViewModelBase_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            HasPropertyChanged = true;
+        }
+    }
+}

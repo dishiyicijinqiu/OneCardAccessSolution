4aa7cb9c9a56c4e2d666490537b43467495fffc5
dishiyicijinqiu <458141536@qq.com>
2016-04-08 12:24:25 +0800

    123

---
+++ BusinessEntity/BasicInfo/RegisterEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FengSharp.OneCardAccess.BusinessEntity.BasicInfo
+{
+    public partial class RegisterEntity
+    {
+        public static RegisterEntity CreateEntity()
+        {
+            return new RegisterEntity();
+        }
+    }
+}
---
+++ Client.PC/Program.cs
@@ -0,0 +1,30 @@
+﻿using FengSharp.OneCardAccess.Client.PC.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FengSharp.OneCardAccess.Client.PC
+{
+    class Program
+    {
+        static System.Threading.Mutex RunMutex;
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            bool isNotRun = false;
+            RunMutex = new System.Threading.Mutex(true, "OneCardAccess.Client.PC", out isNotRun);
+            if (!isNotRun) return;
+            ExceptionHelper.Initialize();
+
+
+            //ThemeManager.ApplicationThemeName = "Office2010Blue";//111
+            //DevExpress.Xpf.Core.ThemeManager.ApplicationThemeName = DevExpress.Xpf.Core.Theme.Office2010BlueName;
+            //DXSplashScreen.Show<SplashScreenView>();
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("zh-cn");
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("zh-cn");
+            App app = new App();
+            app.Run(new MainWindow());
+        }
+    }
+}

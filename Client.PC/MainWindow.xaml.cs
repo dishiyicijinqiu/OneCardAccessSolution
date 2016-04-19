4bdf65ec0a40f85e5cf25b88a6f972c077e25656
dishiyicijinqiu <458141536@qq.com>
2016-04-19 10:07:45 +0800

    窗口最大化不遮挡任务栏，中文语言包

--- Client.PC/MainWindow.xaml.cs
+++ Client.PC/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DevExpress.Xpf.Ribbon;
 using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace FengSharp.OneCardAccess.Client.PC
@@ -12,6 +13,163 @@ namespace FengSharp.OneCardAccess.Client.PC
         public MainWindow()
         {
             InitializeComponent();
+            //this.Width = this.MaxWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            //this.Height = this.MaxHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            //this.Width = System.Windows.SystemParameters.WorkArea.Width;
+            //this.Height = this.MaxHeight = System.Windows.SystemParameters.WorkArea.Height;
+            //this.Left = 0;
+            //this.Top = 30;
+            //this.WindowState = System.Windows.WindowState.Maximized;
+            //this.Topmost = true;
+            this.SourceInitialized += MainWindow_SourceInitialized;
         }
+
+        private void MainWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            System.IntPtr handle = (new System.Windows.Interop.WindowInteropHelper(this)).Handle;
+            System.Windows.Interop.HwndSource.FromHwnd(handle).AddHook(new System.Windows.Interop.HwndSourceHook(new MaximizedNoCoverTaskInfoHelper()
+            {
+                MinWidth = (int)this.MinWidth,
+                MinHeight = (int)this.MinHeight
+            }.WindowProc));
+        }
+    }
+    public class MaximizedNoCoverTaskInfoHelper
+    {
+        public int MinWidth { get; set; }
+        public int MinHeight { get; set; }
+        public int MaxWidth { get; set; }
+        public int MaxHeight { get; set; }
+        public System.IntPtr WindowProc(System.IntPtr hwnd, int msg, System.IntPtr wParam, System.IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case 0x0024:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    handled = true;
+                    break;
+            }
+            return (System.IntPtr)0;
+        }
+        private void WmGetMinMaxInfo(System.IntPtr hwnd, System.IntPtr lParam)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            System.IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+            if (monitor != System.IntPtr.Zero)
+            {
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+
+                mmi.ptMaxTrackSize.x = MaxWidth == 0 ? Math.Abs(rcWorkArea.right - rcWorkArea.left) : MaxWidth;
+                mmi.ptMaxTrackSize.y = MaxHeight == 0 ? Math.Abs(rcWorkArea.bottom - rcWorkArea.top) : MaxHeight;
+
+                mmi.ptMinTrackSize.x = MinWidth == 0 ? Math.Abs(rcWorkArea.right - rcWorkArea.left) : MinWidth;
+                mmi.ptMinTrackSize.y = MinHeight == 0 ? Math.Abs(rcWorkArea.bottom - rcWorkArea.top) : MinHeight;
+            }
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+            public RECT rcMonitor = new RECT();
+            public RECT rcWork = new RECT();
+            public int dwFlags = 0;
+        }
+        /// <summary> Win32 </summary>
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+            public static readonly RECT Empty = new RECT();
+            public int Width
+            {
+                get { return Math.Abs(right - left); }  // Abs needed for BIDI OS
+            }
+            public int Height
+            {
+                get { return bottom - top; }
+            }
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+            public RECT(RECT rcSrc)
+            {
+                this.left = rcSrc.left;
+                this.top = rcSrc.top;
+                this.right = rcSrc.right;
+                this.bottom = rcSrc.bottom;
+            }
+            public bool IsEmpty
+            {
+                get
+                {
+                    return left >= right || top >= bottom;
+                }
+            }
+            public override string ToString()
+            {
+                if (this == RECT.Empty) { return "RECT {Empty}"; }
+                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
+            }
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Rect)) { return false; }
+                return (this == (RECT)obj);
+            }
+            public override int GetHashCode()
+            {
+                return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
+            }
+            public static bool operator ==(RECT rect1, RECT rect2)
+            {
+                return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
+            }
+            public static bool operator !=(RECT rect1, RECT rect2)
+            {
+                return !(rect1 == rect2);
+            }
+        }
+
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        [DllImport("User32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
     }
 }

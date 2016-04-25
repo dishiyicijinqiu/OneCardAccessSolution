b0902df62c7807a3b7c3b8abb85f4cb23cc401d0
dishiyicijinqiu <458141536@qq.com>
2016-04-25 10:16:10 +0800

    11

--- Client.PC/MainWindow.xaml.cs
+++ Client.PC/MainWindow.xaml.cs
@@ -9,22 +9,22 @@ namespace FengSharp.OneCardAccess.Client.PC
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : DXWindow
+    public partial class MainWindow : DXRibbonWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-            //this.SourceInitialized += MainWindow_SourceInitialized;
+            this.SourceInitialized += MainWindow_SourceInitialized;
         }
 
-        //private void MainWindow_SourceInitialized(object sender, EventArgs e)
-        //{
-        //    System.IntPtr handle = (new System.Windows.Interop.WindowInteropHelper(this)).Handle;
-        //    System.Windows.Interop.HwndSource.FromHwnd(handle).AddHook(new System.Windows.Interop.HwndSourceHook(new MaximizedNoCoverTaskInfoHelper()
-        //    {
-        //        MinWidth = (int)this.MinWidth,
-        //        MinHeight = (int)this.MinHeight
-        //    }.WindowProc));
-        //}
+        private void MainWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            System.IntPtr handle = (new System.Windows.Interop.WindowInteropHelper(this)).Handle;
+            System.Windows.Interop.HwndSource.FromHwnd(handle).AddHook(new System.Windows.Interop.HwndSourceHook(new MaximizedNoCoverTaskInfoHelper()
+            {
+                //MinWidth = (int)this.MinWidth,
+                //MinHeight = (int)this.MinHeight
+            }.WindowProc));
+        }
     }
 }

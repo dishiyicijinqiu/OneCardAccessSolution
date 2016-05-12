731324b658b0c1ac90cbf5bae8c8941d2c9f097f
dishiyicijinqiu <458141536@qq.com>
2016-05-12 17:03:30 +0800

    33

--- Client.PC/ViewModel/BasicInfo/RegisterViewModel.cs
+++ Client.PC/ViewModel/BasicInfo/RegisterViewModel.cs
@@ -6,20 +6,30 @@ using FengSharp.OneCardAccess.ServiceInterfaces;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.ViewModel;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Windows.Input;
 
 namespace FengSharp.OneCardAccess.Client.PC.ViewModel.BasicInfo
 {
     public class RegisterViewModel : CrudNotificationObject<RegisterEditMessage, string>
     {
-
+        #region commands
         public ICommand SaveAndNewCommand { get; private set; }
         public ICommand SaveCommand { get; private set; }
         public ICommand UpLoadRegisterFileCommand { get; private set; }
+        public ICommand MoveUpRegisterFileCommand { get; private set; }
+        public ICommand MoveDownRegisterFileCommand { get; private set; }
+        public ICommand DeleteRegisterFileCommand { get; private set; }
+        public ICommand AddRegisterTempCommand { get; private set; }
+        public ICommand MoveUpRegisterTempCommand { get; private set; }
+        public ICommand MoveDownRegisterTempCommand { get; private set; }
+        public ICommand DeleteRegisterTempCommand { get; private set; }
         public ICommand ViewRegisterFileCommand { get; private set; }
+        #endregion
         public RegisterViewModel(object ParentViewModel, RegisterEditMessage editmessage)
         {
             this.ParentViewModel = ParentViewModel;
@@ -27,8 +37,14 @@ namespace FengSharp.OneCardAccess.Client.PC.ViewModel.BasicInfo
             SaveAndNewCommand = new DelegateCommand(SaveAndNew);
             SaveCommand = new DelegateCommand(Save);
             UpLoadRegisterFileCommand = new DelegateCommand(UpLoadRegisterFile);
+            MoveUpRegisterFileCommand = new DelegateCommand<Register_FileEntity>(MoveUpRegisterFile);
+            MoveDownRegisterFileCommand = new DelegateCommand<Register_FileEntity>(MoveDownRegisterFile);
+            DeleteRegisterFileCommand = new DelegateCommand<IList>(DeleteRegisterFile);
+            AddRegisterTempCommand = new DelegateCommand(AddRegisterTemp);
+            MoveUpRegisterTempCommand = new DelegateCommand<FirstP_CRTempEntity>(MoveUpRegisterTemp);
+            MoveDownRegisterTempCommand = new DelegateCommand<FirstP_CRTempEntity>(MoveDownRegisterTemp);
+            DeleteRegisterTempCommand = new DelegateCommand<IList>(DeleteRegisterTemp);
             ViewRegisterFileCommand = new DelegateCommand<Register_FileEntity>(ViewRegisterFile);
-             
             Entity = SecondRegisterEntity.CreateEntity();
             if (this.EditMessage == null)
                 throw new Exception(Properties.Resources.Error_ParameterIsError);
@@ -52,6 +68,7 @@ namespace FengSharp.OneCardAccess.Client.PC.ViewModel.BasicInfo
                 case EntityEditMode.Edit:
                     {
                         var newEntity = ServiceProxyFactory.Create<IBasicInfoService>().GetSecondRegisterEntityById(this.EditMessage.Key);
+                        newEntity.Register_FileEntitys = new ObservableCollection<Register_FileEntity>(newEntity.Register_FileEntitys.OrderBy(t => t.SortNo));
                         Entity.CopyValueFrom(newEntity);
                     }
                     break;
@@ -74,7 +91,7 @@ namespace FengSharp.OneCardAccess.Client.PC.ViewModel.BasicInfo
             }
         }
         #endregion
-
+        #region commandmethods
         private void ViewRegisterFile(Register_FileEntity fileentity)
         {
             try
@@ -120,6 +137,133 @@ namespace FengSharp.OneCardAccess.Client.PC.ViewModel.BasicInfo
                 ShowException(ex);
             }
         }
+
+        private void DeleteRegisterTemp(IList entitys)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                ShowException(ex);
+            }
+        }
+
+        private void MoveDownRegisterTemp(FirstP_CRTempEntity obj)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                ShowException(ex);
+            }
+        }
+
+        private void MoveUpRegisterTemp(FirstP_CRTempEntity obj)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                ShowException(ex);
+            }
+        }
+
+        private void AddRegisterTemp()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                ShowException(ex);
+            }
+        }
+
+        private void DeleteRegisterFile(IList entitys)
+        {
+            try
+            {
+                if (entitys == null || entitys.Count <= 0)
+                {
+                    ShowMessage(Properties.Resources.Info_SelectAtLeastOne);
+                    return;
+                }
+                var deleteArgs = new MessageBoxEventArgs(Properties.Resources.Info_ConfirmToDelete, Properties.Resources.Info_Title, MsgButton.YesNo, MsgImage.Information);
+                if (ShowMessage(deleteArgs) != MsgResult.Yes)
+                    return;
+                for (int i = entitys.Count-1; i >=0; i--)
+                {
+                    this.Entity.Register_FileEntitys.Remove(entitys[i] as Register_FileEntity);
+                }
+                for (int i = 0; i <= this.Entity.Register_FileEntitys.Count - 1; i++)
+                {
+                    this.Entity.Register_FileEntitys[i].SortNo = i;
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowException(ex);
+            }
+        }
+
+        private void MoveDownRegisterFile(Register_FileEntity entity)
+        {
+            try
+            {
+                if (entity == null)
+                {
+                    ShowMessage(Properties.Resources.Info_SelectAtLeastOne);
+                    return;
+                }
+                int count = this.Entity.Register_FileEntitys.Count;
+                int oldindex = this.Entity.Register_FileEntitys.IndexOf(entity);
+                int newindex = oldindex + 1;
+                if (newindex >= count)
+                    return;
+                this.Entity.Register_FileEntitys.Move(oldindex, newindex);
+                for (int i = 0; i <= this.Entity.Register_FileEntitys.Count - 1; i++)
+                {
+                    this.Entity.Register_FileEntitys[i].SortNo = i;
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowException(ex);
+            }
+        }
+
+        private void MoveUpRegisterFile(Register_FileEntity entity)
+        {
+            try
+            {
+
+                if (entity == null)
+                {
+                    ShowMessage(Properties.Resources.Info_SelectAtLeastOne);
+                    return;
+                }
+                int oldindex = this.Entity.Register_FileEntitys.IndexOf(entity);
+                int newindex = oldindex - 1;
+                if (newindex < 0)
+                    return;
+                this.Entity.Register_FileEntitys.Move(oldindex, newindex);
+                for (int i = 0; i <= this.Entity.Register_FileEntitys.Count - 1; i++)
+                {
+                    this.Entity.Register_FileEntitys[i].SortNo = i;
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowException(ex);
+            }
+        }
         public void SaveAndNew()
         {
             this.EditMessage.IsContinue = true;
@@ -131,6 +275,8 @@ namespace FengSharp.OneCardAccess.Client.PC.ViewModel.BasicInfo
             if (this.SaveCore())
                 this.Close();
         }
+        #endregion
+        #region methods
         bool SaveCore()
         {
             try
@@ -151,5 +297,6 @@ namespace FengSharp.OneCardAccess.Client.PC.ViewModel.BasicInfo
                 return false;
             }
         }
+        #endregion
     }
 }

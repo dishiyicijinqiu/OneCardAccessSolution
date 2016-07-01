ec43865c2b43f2f21bd82b4a6d40d5fb7046006f
SZKL-Developer <SZKL-Developer@SZKL-DEVELOP>
2016-07-01 13:48:04 +0800

    111

--- WebSite/Scripts/OneCardAccess.js
+++ WebSite/Scripts/OneCardAccess.js
@@ -1,16 +1,24 @@
 ﻿$(function () {
-    $(document).ready(function () {
-        $(document).contextmenu(function () {
-            return false;
-        });
-        //$("div").select(function () {
-        //    return false;
-        //});
-        //$("div").disableSelection();
-        //$("a").disableSelection();
-        //$("*").attr('unselectable', 'on');
-        //$("input").attr('unselectable', 'off');
-        $("*").disableSelection();
-        //$("input").enableSelection();
+    $(document).contextmenu(function () {
+        return false;
     });
+    //$("div").select(function () {
+    //    return false;
+    //});
+    //$("div").disableSelection();
+    //$("a").disableSelection();
+    //$("*").attr('unselectable', 'on');
+    //$("input").attr('unselectable', 'off');
+    $("*").disableSelection();
+    //$("input").enableSelection();
 });
+
+
+
+//var d = new Date(), str = '';
+//str += d.getFullYear() + '年';
+//str += d.getMonth() + 1 + '月';
+//str += d.getDate() + '日';
+//str += d.getHours() + '时';
+//str += d.getMinutes() + '分';
+//str += d.getSeconds() + '秒';
---
+++ WebSite/Scripts/Struct.js
@@ -0,0 +1,40 @@
+﻿//-------------ViewBody
+//外层内高度-固定(div)外高度-(自适应(div)内外间距)=自适应(div)高度
+function adjustHeight() {
+    var cheight = $(document).innerHeight() - $("#ViewBody_Header").outerHeight(true) - $("#ViewBody_Footer").outerHeight(true);
+    var iospaceheight = $("#ViewBody_Content").outerHeight(true) - $("#ViewBody_Content").innerHeight();
+    $("#ViewBody_Content").css("height", cheight + "px");
+}
+//-------------ContentsPartialView
+function adjustContentHeight() {
+    var iospaceheight = $("#navmenu").outerHeight(true) - $("#navmenu").innerHeight();
+    var cheight = $("#tabs").innerHeight() - $("#navmenu").outerHeight(true) - iospaceheight;
+    $("#tab-content").css("height", cheight + "px");
+}
+//-------------FooterPartialView
+function OnRibbonResize(s, e) {
+    adjustHeight();
+    adjustContentHeight();
+}
+
+$(function () {
+    //-------------ViewBody
+    $(window).bind("resize", function () {
+        adjustHeight();
+    });
+    //$(window).resize(function () {
+    //    adjustHeight();
+    //});
+    adjustHeight();
+
+    //-------------ContentsPartialView
+    setTimeout(function () {
+        adjustContentHeight();
+    }, 10);
+    $(window).bind("resize", function () {
+        adjustContentHeight();
+    });
+
+});
+
+
